82a0179cf5c427cd960dc5d4175ae38cef3e16d0
Steve Njuguna <stevenjuguna93@gmail.com>
2021-09-30 16:42:03 +0300

    Added urls, made changes to models.py & settings.py

--- src/Products/models.py
+++ src/Products/models.py
@@ -5,7 +5,7 @@ class Products(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     quantity = models.IntegerField()
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
 
     def __str__(self):
         return self.name
