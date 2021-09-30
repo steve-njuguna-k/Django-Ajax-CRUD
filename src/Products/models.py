dac723519c096848ed3dc7c2365355e0498830cb
Steve Njuguna <stevenjuguna93@gmail.com>
2021-09-30 16:00:47 +0300

    Added Products model

--- src/Products/models.py
+++ src/Products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    price = models.DecimalField(decimal_places=2)
+
+    def __str__(self):
+        return self.name
